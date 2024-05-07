ee7fb12ee469747a0ba81aee0e6b6157690ee6a6
George <lwhittington2231@gmail.com>
2024-05-06 22:11:36 -0400

    Frog now has gravity

--- scripts/frog_enemy.gd
+++ scripts/frog_enemy.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _physics_process(delta):
+	# gravity for frog
+	velocity.y += gravity * delta
+	
+	move_and_slide()
+
 func _on_player_detection_body_entered(body):
 	if body.name == "Player":
 		print("player")
