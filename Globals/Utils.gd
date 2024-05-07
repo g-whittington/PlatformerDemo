3ba3b55bea8442094ddd5925eddb74fd2a5ecd13
George <lwhittington2231@gmail.com>
2024-05-07 18:13:42 -0400

    Added save and load funcions to utils

--- Globals/Utils.gd
+++ Globals/Utils.gd
@@ -1,1 +1,23 @@
 extends Node
+
+
+# Should and can use users:// instead of res
+const SAVE_PATH = "res://savegame.bin"
+
+func save_game():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	var data: Dictionary = {
+		"playerHP": Game.playerHP,
+		"gold": Game.gold
+	}
+	var jstr = JSON.stringify(data)
+	file.store_line(jstr)
+	
+func load_game():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if FileAccess.file_exists(SAVE_PATH):
+		if not file.eof_reached():
+			var current_line = JSON.parse_string(file.get_line())
+			if current_line:
+				Game.playerHP = current_line["playerHP"]
+				Game.gold = current_line["gold"]
